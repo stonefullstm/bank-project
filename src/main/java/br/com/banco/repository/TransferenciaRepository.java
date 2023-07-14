2bd0c555ed008d0e092dd40a5033e4c70d9737cd
stonefullstm <stonefull.stm@gmail.com>
2023-07-14 17:32:06 -0300

    feat: Criação de métodos query em TransferenciaRepository

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -2,9 +2,16 @@ package br.com.banco.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import br.com.banco.model.Conta;
 import br.com.banco.model.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
   List<Transferencia> findByConta(Conta conta);
+
+  @Query("SELECT SUM(t.valor) from Transferencia t where t.conta= ?1")
+  double getSaldoTotal(Conta conta);
+
+  List<Transferencia> findByContaAndNomeOperadorTransacao(Conta conta,
+      String nomeOperadorTransacao);
 }
