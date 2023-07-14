29fda1eab903bde81480dab220d2a48ed48b4a26
stonefullstm <stonefull.stm@gmail.com>
2023-07-14 18:35:05 -0300

    feat: Inclusão de parâmetros na rota GET /transferencias/id

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -1,13 +1,19 @@
 package br.com.banco.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.banco.model.Extrato;
 import br.com.banco.model.Transferencia;
 import br.com.banco.service.ContaService;
 import br.com.banco.service.TransferenciaService;
@@ -28,10 +34,24 @@ public class TransferenciaController {
     return ResponseEntity.status(HttpStatus.OK).body(transferencias);
   }
 
+  @CrossOrigin
   @GetMapping("/{id}")
-  public ResponseEntity<List<Transferencia>> findByConta(@PathVariable("id") Long id) {
+  public ResponseEntity<Extrato> findByConta(@PathVariable("id") Long id,
+      @RequestParam Optional<String> operador, @RequestParam Optional<Date> datainicial,
+      @RequestParam Optional<Date> datafinal) {
     var conta = this.contaService.findById(id);
-    List<Transferencia> transferencias = this.transferenciaService.findByConta(conta);
-    return ResponseEntity.status(HttpStatus.OK).body(transferencias);
+    var saldo = this.transferenciaService.getSaldoTotal(conta);
+    List<Transferencia> transferencias = new ArrayList<Transferencia>();
+    if (operador.isEmpty() && datainicial.isEmpty() && datafinal.isEmpty()) {
+      transferencias = this.transferenciaService.findByConta(conta);
+    } else if (datainicial.isEmpty() && datafinal.isEmpty()) {
+      transferencias =
+          this.transferenciaService.findByContaAndNomeOperadorTransacao(conta, operador.get());
+    } else if (datafinal.isEmpty()) {
+      transferencias =
+          this.transferenciaService.findByContaAndDataTransferenciaAfter(conta, datainicial.get());
+    }
+    Extrato extrato = new Extrato(saldo, transferencias);
+    return ResponseEntity.status(HttpStatus.OK).body(extrato);
   }
 }
